b82c1a2f6df9c49091a9a0fecd1836a88704703f
chawan <chawandossantos7@gmail.com>
2025-01-21 15:18:53 -0300

    Teste importando foto asserts

--- src/components/ComponentHome/index.jsx
+++ src/components/ComponentHome/index.jsx
@@ -2,25 +2,30 @@
 import './style.css'
 import Carousel from '../Carousel/index.jsx'
 
+import foto1 from '../../assets/fotomascarelofrente1080.jpg'
+import foto2 from '../../assets/fotocontact.png'
+import foto3 from '../../assets/foto-equipe-parceiros.jpg'
+import foto4 from '../../assets/foto-internabus1080.jpg'
+
 export default function ComponentHome() {
     const data = [
         {
-            image: "../../assets/fotomascarelofrente1080.jpg",
+            image: foto1,
             title: "Bem-vindo ao Nosso Sistema",
             description: "A solução completa para transporte e inovação.",
         },
         {
-            image: "../../assets/fotocontact.png",
+            image: foto2,
             title: "Notícias Recentes",
             description: "Fique por dentro das vagas disponiveis.",
         },
         {
-            image: "../../assets/foto-equipe-parceiros.jpg",
+            image: foto3,
             title: "Equipe Dedicada",
             description: "Conheça nossa equipe e parceiros de sucesso.",
         },
         {
-            image: "../../assets/foto-internabus1080.jpg",
+            image: foto4,
             title: "Inovação em Movimento",
             description: "Transformando o transporte para um futuro melhor.",
         },
